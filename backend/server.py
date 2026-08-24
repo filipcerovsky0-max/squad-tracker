e0f21bbd661230b2e4d4da1cbd006503cb6999a5
Filispin <filipcerovsky0@gmail.com>
2026-08-24 16:16:40 +0200

    oprava GA4 - spravne umisteni + cookie consent gating

--- backend/server.py
+++ backend/server.py
@@ -909,7 +909,7 @@ CSP_POLICY = (
     "style-src 'self' 'unsafe-inline' https://unpkg.com https://fonts.googleapis.com; "
     "font-src 'self' https://fonts.gstatic.com; "
     "img-src 'self' data: blob: https://*.tile.openstreetmap.org https://server.arcgisonline.com https://api.qrserver.com; "
-    "connect-src 'self' https://www.googletagmanager.com; "
+    "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com; "
     "frame-src https://www.googletagmanager.com; "
     "frame-ancestors 'none'; "
     "object-src 'none'; "
